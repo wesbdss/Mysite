fd7e90f91570be75ae6a9d9038c07a8a33844539
wesbdss <wesleybenicio4@gmail.com>
2020-09-12 18:15:06 -0300

    ta

--- inicio/urls.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),# name='' é o nome da classe em views.py
-]
--- inicio/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    var = "'Teste de passagem de variavel'"
-    return render(request,'index.html',{'variavel' : var})
